8c357c633e196c2a7a28e9284a2994e25031d22a
McKay1717 <nicolas1706ni@gmail.com>
2016-05-03 15:38:18 +0200

    Modification de la class GrilleDeJeuJpanel(Constructeur + attribut) Création de la class principal (Abscance de controleur)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import engine.GrilleJeux;
+
 /**
  * 
  */
@@ -10,10 +12,17 @@ public class App {
 
 	/**
 	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+*/
+		public static void main(String[] args) {
+
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					GrilleJeux gj = new GrilleJeux();
+					//Controleur GUI ?
+					//ControlGroup control = new ControlGroup(model);
+				}
+			});
+		}
 
 	}
 
-}
--- src/main/java/gui/GrilleDeJeuJPanel.java
+++ src/main/java/gui/GrilleDeJeuJPanel.java
@@ -3,6 +3,8 @@
  *******************************************************************************/
 package gui;
 
+import java.awt.GridLayout;
+
 // Start of user code (user defined imports)
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,31 +17,38 @@ import engine.GrilleJeux;
  * 
  * @author nicolas
  */
-public class GrilleDeJeuJPanel extends JPanel
-{
+public class GrilleDeJeuJPanel extends JPanel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -416871502500554822L;
-	public static final int TAILLE_GRILLE = 10;															//Taille des lignes et colonnes de la grille.
-	public static final char[] INTITULE_LIGNE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};		//Intitulé des lignes.
+	public static final int TAILLE_GRILLE = 10; // Taille des lignes et colonnes
+												// de la grille.
+	public static final char[] INTITULE_LIGNE = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' }; // Intitulé
+																										// des
+																										// lignes.
 
 	// Start of user code (user defined attributes for GrilleDeJeuJPanel)
-	public CaseJButton[][] grille;		//Grille de JButton.
-	public JLabel[] intitule_ligne;		//Intitulé des lignes.
-	public JLabel[] intitule_colonne;	//Intitulé des colonnes.
-	public CaseListener control_button;	//Contrôleur de bouton.
+	public CaseJButton[][] grille; // Grille de JButton.
+	public JLabel[] intitule_ligne = new JLabel[TAILLE_GRILLE]; // Intitulé des
+																// lignes.
+	public JLabel[] intitule_colonne = new JLabel[TAILLE_GRILLE];; // Intitulé
+																	// des
+																	// colonnes.
+	public CaseListener control_button; // Contrôleur de bouton.
 	// End of user code
+	private GrilleJeux grille_jeu;
 
 	/**
 	 * The constructor.
 	 */
-	public GrilleDeJeuJPanel()
-	{
+	public GrilleDeJeuJPanel(GrilleJeux gj) {
 		// Start of user code constructor for GrilleDeJeuJPanel)
-		control_button = new CaseListener(this);	//Initialisation du contrôleur de bouton.
-		initAttribut();								//Initialisation des attributs.
-		creerWidget();								//Initialisation de la fenêtre.
+		this.grille_jeu = gj;
+		control_button = new CaseListener(this); // Initialisation du contrôleur
+													// de bouton.
+		initAttribut(); // Initialisation des attributs.
+		creerWidget(); // Initialisation de la fenêtre.
 		// End of user code
 	}
 
@@ -48,34 +57,33 @@ public class GrilleDeJeuJPanel extends JPanel
 	/**
 	 * Initialisation de la grille.
 	 */
-	public void initAttribut()
-	{
-		grille = new CaseJButton[TAILLE_GRILLE][TAILLE_GRILLE];		//Initialisation du tableau de CaseJButton.
+	public void initAttribut() {
+		grille = new CaseJButton[TAILLE_GRILLE][TAILLE_GRILLE]; // Initialisation
+																// du tableau de
+																// CaseJButton.
 
-		//Parcours de la grille pour initialiser chaque case.
-		for (int i = 0 ; i < TAILLE_GRILLE ; i++)
-			for (int j = 0 ; j < TAILLE_GRILLE ; j++)
+		// Parcours de la grille pour initialiser chaque case.
+		for (int i = 0; i < TAILLE_GRILLE; i++)
+			for (int j = 0; j < TAILLE_GRILLE; j++)
 				grille[i][j] = new CaseJButton(grille_jeu, i, j);
 
-		//Initialisation de l'intitulé des lignes.
-		for (int i = 0 ; i < TAILLE_GRILLE ; i++)
+		// Initialisation de l'intitulé des lignes.
+		for (int i = 0; i < TAILLE_GRILLE; i++)
 			intitule_ligne[i] = new JLabel(Character.toString(INTITULE_LIGNE[i]));
 
-		//Initialisation de l'intitulé des colonnes.
-		for (int i = 0 ; i < TAILLE_GRILLE ; i++)
-			//Erreur, c'est quoi le but de code ?
-			intitule_colonne = new JLabel(Integer.toString(i + 1));
+		// Initialisation de l'intitulé des colonnes.
+		for (int i = 0; i < TAILLE_GRILLE; i++)
+			// Erreur, c'est quoi le but de code ?
+			intitule_colonne[i] = new JLabel(Integer.toString(i + 1));
 	}
 
 	/**
 	 * Création de la fenêtre.
 	 */
-	public void creerWidget()
-	{
+	public void creerWidget() {
 		JPanel pGrille = new JPanel(new GridLayout(TAILLE_GRILLE + 1, TAILLE_GRILLE + 1));
-		for (int i = 0 ; i < TAILLE_GRILLE + 1 ; i++)
-			for (int j = 0 ; j < TAILLE_GRILLE + 1 ; j++)
-			{
+		for (int i = 0; i < TAILLE_GRILLE + 1; i++)
+			for (int j = 0; j < TAILLE_GRILLE + 1; j++) {
 				if (i == 0 && j != 0)
 					pGrille.add(intitule_colonne[j - 1]);
 				else if (i != 0 && j == 0)
